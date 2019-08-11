6b1251e01f93ed66c13d09051cc1844ed30e6631
Lenk <jmvg.comp@gmail.com>
2019-08-11 00:29:49 -0300

    Commit inicial com importação dos arquivos html, webjars incluidos, e configurações básicas de teste de url no Controller da Home e Departamento

---
+++ src/main/java/dev/jmvg/controller/HomeController.java
@@ -0,0 +1,12 @@
+package dev.jmvg.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    String home(){
+        return "/home";
+    }
+}
