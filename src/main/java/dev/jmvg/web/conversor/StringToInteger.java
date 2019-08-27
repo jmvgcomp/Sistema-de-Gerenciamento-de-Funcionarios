a97a9f42664926d15b5b605df376392ddd524a69
Lenk <jmvg.comp@gmail.com>
2019-08-26 23:42:29 -0300

    Implementação da conversão de String para Integer no campo número, bem como a sua validação.

---
+++ src/main/java/dev/jmvg/web/conversor/StringToInteger.java
@@ -0,0 +1,16 @@
+package dev.jmvg.web.conversor;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StringToInteger implements Converter<String, Integer> {
+    @Override
+    public Integer convert(String text) {
+        text = text.trim();
+        if(text.matches("[0-9]+")){
+            return Integer.valueOf(text);
+        }
+        return null;
+    }
+}
